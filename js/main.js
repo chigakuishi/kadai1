64cee8021adafe1e466f3412a8f35487a51a4896
rhenium <chigakuihis.soft@gmail.com>
2016-07-05 13:19:01 +0900

    ボタン設置完了

--- js/main.js
+++ js/main.js
@@ -36,165 +36,184 @@ window.onload = function(){
 	var total = 0;
 	// データの読み込みが完了したら処理
 	game.onload = function(){
-		// 自機の設定
-		var fighter = new Sprite(64, 30);
-		fighter.image = game.assets["images/base.png"];
-		fighter.x = game.width/2;	// X座標
-		fighter.y = game.height - 60;	// Y座標
-		fighter._style.zIndex = 1;
-		game.rootScene.addChild(fighter);
-		// ビームの設定
-		var beam = new Sprite(32, 64);
-		beam.flag = false;	// ビームが発射されているかどうかのフラグ
-		beam.x = fighter.x + 20;	// 自機の中央に設置
-		beam.y = fighter.y - 45;	// 自機より少し上のY座標に設置
-		beam.image = game.assets["images/beam.png"];
-		beam._style.zIndex = 2;
-		game.rootScene.addChild(beam);
-		// ZキーをAボタンとして割り当てる
-		game.keybind(90, "a");
-		// 敵全体を移動させるための座標を用意する
-		var enemyDX = 3;
-		var enemyDY = 0;
-		// 敵を描く
-		drawEnemy();
-		// 敵のレーザービームを初期化する
-		initEnemyLaser();
-		// 爆風の設定
-		var blast = new Sprite(40, 40);
-		blast.image = game.assets["images/effect0.png"];
-		blast.y = -9999;	// Y座標
-		blast._style.zIndex = 10;	// 爆風は一番手前に重なるようにする
-		game.rootScene.addChild(blast);
-		// フレームイベントが発生したら処理
-		game.rootScene.addEventListener(Event.ENTER_FRAME, function(){
-			startBeam();		// ビームの発射を確認
-			moveBeam();		// ビームを移動させる
-			moveFighter();	// 自機を移動させる（キーボード対応）
-			moveEnemy();		// 敵を移動させる
-			startEnemyLaser();	//敵のレーザービームを発射する
-			moveEnemyLaser();		// 敵のレーザービームを移動させる
-			moveBlast();		// 爆発の処理を行う
-			hitCheck();	// ビームと敵の接触判定
-			hitCheckLaser();	// レーザービームと自機の接触判定
+		//button関係
+		var button = new Button("スタート!", "light");
+		button.moveTo(10,50);
+		game.rootScene.addChild(button);
+		var resetButton = new Button("リセット!", "light");
+		resetButton.moveTo(100,0);
+		game.rootScene.addChild(resetButton);
+		resetButton.ontouchstart = function(){
+			location.reload();
+		}
+		var introButton = new Button("説明", "light");
+		introButton.moveTo(10,100);
+		game.rootScene.addChild(introButton);
+		introButton.ontouchstart = function(){
+			location.href="intro.html";
+		}
+		button.ontouchstart = function(){
+			game.rootScene.removeChild(button);
+			game.rootScene.removeChild(introButton);
+			// 自機の設定
+			var fighter = new Sprite(64, 30);
+			fighter.image = game.assets["images/base.png"];
+			fighter.x = game.width/2;	// X座標
+			fighter.y = game.height - 60;	// Y座標
+			fighter._style.zIndex = 1;
+			game.rootScene.addChild(fighter);
+			// ビームの設定
+			var beam = new Sprite(32, 64);
+			beam.flag = false;	// ビームが発射されているかどうかのフラグ
+			beam.x = fighter.x + 20;	// 自機の中央に設置
+			beam.y = fighter.y - 45;	// 自機より少し上のY座標に設置
+			beam.image = game.assets["images/beam.png"];
+			beam._style.zIndex = 2;
+			game.rootScene.addChild(beam);
+			// ZキーをAボタンとして割り当てる
+			game.keybind(90, "a");
+			// 敵全体を移動させるための座標を用意する
+			var enemyDX = 3;
+			var enemyDY = 0;
+			// 敵を描く
+			drawEnemy();
+			// 敵のレーザービームを初期化する
+			initEnemyLaser();
+			// 爆風の設定
+			var blast = new Sprite(40, 40);
+			blast.image = game.assets["images/effect0.png"];
+			blast.y = -9999;	// Y座標
+			blast._style.zIndex = 10;	// 爆風は一番手前に重なるようにする
+			game.rootScene.addChild(blast);
+			// フレームイベントが発生したら処理
+			game.rootScene.addEventListener(Event.ENTER_FRAME, function(){
+				startBeam();		// ビームの発射を確認
+				moveBeam();		// ビームを移動させる
+				moveFighter();	// 自機を移動させる（キーボード対応）
+				moveEnemy();		// 敵を移動させる
+				startEnemyLaser();	//敵のレーザービームを発射する
+				moveEnemyLaser();		// 敵のレーザービームを移動させる
+				moveBlast();		// 爆発の処理を行う
+				hitCheck();	// ビームと敵の接触判定
+				hitCheckLaser();	// レーザービームと自機の接触判定
 
-			// =============== 各種処理 ==================
-			// ------------ ■ビームを移動させる -----------------
-			function moveBeam(){
-				if (beam.flag){
-					beam.y = beam.y - 8;	// 8を減算するとビームは上に移動する
-					// 画面外かどうか調べる
-					if (beam.y < -32){ beam.flag = false; }
-				}
-			}
-			// ------------ ■ビームを発射する -----------------
-			function startBeam(){
-				if (!beam.flag){
-					// Aボタンが押されたらビームを発射
-					if (game.input.a){
-						beam.flag = true;	// trueにしてビームが発射されている事を示すようにする
-						beam.x = fighter.x + 20;	// 自機の中央から出す
-						beam.y = fighter.y - 45;	// 自機より少し上のY座標から出す
+				// =============== 各種処理 ==================
+				// ------------ ■ビームを移動させる -----------------
+				function moveBeam(){
+					if (beam.flag){
+						beam.y = beam.y - 8;	// 8を減算するとビームは上に移動する
+						// 画面外かどうか調べる
+						if (beam.y < -32){ beam.flag = false; }
 					}
 				}
-			}
-			// ------------ ■自機を移動させる -----------------
-			function moveFighter(){
-				// キーボード操作の場合
-				if (game.input.left){
-					fighter.x = fighter.x - 4;	// パドルを左に移動
-					if (fighter.x < 0){ fighter.x = 0; }	// 左端かどうか調べる
-				}
-				if (game.input.right){
-					fighter.x = fighter.x + 4;	 // パドルを右に移動
-					if (fighter.x > (game.width-fighter.width)){ fighter.x = game.width - fighter.width; }	// 右端かどうか調べる
-				}
-				// ビームが発射されていない場合は自機と一緒に移動
-				if (!beam.flag){
-					beam.x = fighter.x + 20;	// 自機の中央に設置
-					beam.y = fighter.y - 45;	// 自機より少し上のY座標に設置
-				}
-			}
-			// ------------ ■敵を移動させる -----------------
-			function moveEnemy(){
-				var reverseFlag = false;
-				for(var i=0; i<count; i++){
-					enemy[i].x = enemy[i].x + enemyDX;	// X座標の移動処理
-					enemy[i].y= enemy[i].y + enemyDY;	// Y座標の移動処理
-					enemy[i].tick = enemy[i].tick + 1;
-					enemy[i].frame = enemy[i].tick >>> 4;	// ★敵をアニメーションさせる
-					// 左右の端に到達したか調べる
-					if ((enemy[i].y > 0) && ((enemy[i].x < 0) || (enemy[i].x > 330))){ reverseFlag = true; }
-					if (enemy[i].y > 415){
-						game.rootScene.backgroundColor = "red";	// ゲームの背景色を赤色に設定
-						game.stop();
-						alert("A帝国はG帝国に征服されました。スコアは"+game.score+"点でした");
-						return;	// 以後の処理は行わないようにする
-					}
-				}
-				// 左右どちらかの端に到達した敵がいた場合の処理
-				if (reverseFlag){
-					enemyDX = -enemyDX;
-					enemyDY = 6;
-				}else{
-					enemyDY = 0;
-				}
-			}
-			// ------------ ■敵とビームの接触判定を行う -----------------
-			function hitCheck(){
-				if (!beam.flag){ return; }	// ビームが発射されていない場合は処理しない
-				for(var i=0; i<count; i++){
-					if (beam.intersect(enemy[i])){
-						startBlast(enemy[i].x, enemy[i].y);	// ★爆風発生
-						beam.flag = false;	// 接触した場合はビームを消す
-						enemy[i].y = -9999;	// 見えない場所に移動
-						game.score = game.score + 1;	// スコアを加算(1点)
-						total = total - 1;	// 総敵数から1を引く
-						if (total < 1){	// 全部倒したか調べる
-							setTimeout("drawEnemy()", 2000);	// 2秒後に敵を再描画
-							maxStartLaser = maxStartLaser + 4;	// レーザービームの数を4つずつ増やす
-							if (maxStartLaser > 40){ maxStartLaser = 40; }	// 最大40まで
-							rank = rank - 10;
-							if (rank < maxStartLaser){ rank = maxStartLaser; }	// 難易度調整
-							stage = stage + 1;	// ステージを1つ増やす
-							stageLabel.text = "STAGE "+stage;
+				// ------------ ■ビームを発射する -----------------
+				function startBeam(){
+					if (!beam.flag){
+						// Aボタンが押されたらビームを発射
+						if (game.input.a){
+							beam.flag = true;	// trueにしてビームが発射されている事を示すようにする
+							beam.x = fighter.x + 20;	// 自機の中央から出す
+							beam.y = fighter.y - 45;	// 自機より少し上のY座標から出す
 						}
 					}
 				}
-				scoreLabel.text = "SCORE : "+game.score;
-			}
-			// ------------ ■自機と敵のレーザービームの接触判定を行う -----------------
-			function hitCheckLaser(){
-				// 自機の判定用に仮のオブジェクトを作成しXY座標と幅を設定する
-
-				for(var i=0; i<maxLaser; i++){
-					if (enemyLaser[i].intersect(fighter)){	// 接触したらゲームオーバー
-						game.rootScene.backgroundColor = "red";	// ゲームの背景色を赤色に設定
-						game.stop();
-						alert("自機が破壊されました。もう駄目です。スコアは"+game.score+"点でした");
-						return;	// 以後の処理は行わないようにする
+				// ------------ ■自機を移動させる -----------------
+				function moveFighter(){
+					// キーボード操作の場合
+					if (game.input.left){
+						fighter.x = fighter.x - 4;	// パドルを左に移動
+						if (fighter.x < 0){ fighter.x = 0; }	// 左端かどうか調べる
+					}
+					if (game.input.right){
+						fighter.x = fighter.x + 4;	 // パドルを右に移動
+						if (fighter.x > (game.width-fighter.width)){ fighter.x = game.width - fighter.width; }	// 右端かどうか調べる
+					}
+					// ビームが発射されていない場合は自機と一緒に移動
+					if (!beam.flag){
+						beam.x = fighter.x + 20;	// 自機の中央に設置
+						beam.y = fighter.y - 45;	// 自機より少し上のY座標に設置
 					}
 				}
-			}
-
-			// ■爆発開始処理
-			function startBlast(sx, sy){
-				blast.flag = true;	// 爆風発生
-				blast.x = sx;
-				blast.y = sy;
-				blast.frame = 0;	// 爆風のアニメーションを最初の画像に
-			}
-			// ■爆発処理
-			function moveBlast(){
-				if (!blast.flag){ return; }	// 爆風がない場合は何もしない
-				blast.frame = blast.frame + 1;
-				if (blast.frame == 8){	// 爆風の枚数は8枚なので、それ以上の場合は爆風処理をしない
-					blast.flag = false;
-					blast.y = -999;
+				// ------------ ■敵を移動させる -----------------
+				function moveEnemy(){
+					var reverseFlag = false;
+					for(var i=0; i<count; i++){
+						enemy[i].x = enemy[i].x + enemyDX;	// X座標の移動処理
+						enemy[i].y= enemy[i].y + enemyDY;	// Y座標の移動処理
+						enemy[i].tick = enemy[i].tick + 1;
+						enemy[i].frame = enemy[i].tick >>> 4;	// ★敵をアニメーションさせる
+						// 左右の端に到達したか調べる
+						if ((enemy[i].y > 0) && ((enemy[i].x < 0) || (enemy[i].x > 330))){ reverseFlag = true; }
+						if (enemy[i].y > 415){
+							game.rootScene.backgroundColor = "red";	// ゲームの背景色を赤色に設定
+							game.stop();
+							alert("A帝国はG帝国に征服されました。スコアは"+game.score+"点でした");
+							return;	// 以後の処理は行わないようにする
+						}
+					}
+					// 左右どちらかの端に到達した敵がいた場合の処理
+					if (reverseFlag){
+						enemyDX = -enemyDX;
+						enemyDY = 6;
+					}else{
+						enemyDY = 0;
+					}
 				}
-			}
-		});
+				// ------------ ■敵とビームの接触判定を行う -----------------
+				function hitCheck(){
+					if (!beam.flag){ return; }	// ビームが発射されていない場合は処理しない
+					for(var i=0; i<count; i++){
+						if (beam.intersect(enemy[i])){
+							startBlast(enemy[i].x, enemy[i].y);	// ★爆風発生
+							beam.flag = false;	// 接触した場合はビームを消す
+							enemy[i].y = -9999;	// 見えない場所に移動
+							game.score = game.score + 1;	// スコアを加算(1点)
+							total = total - 1;	// 総敵数から1を引く
+							if (total < 1){	// 全部倒したか調べる
+								setTimeout("drawEnemy()", 2000);	// 2秒後に敵を再描画
+								maxStartLaser = maxStartLaser + 4;	// レーザービームの数を4つずつ増やす
+								if (maxStartLaser > 40){ maxStartLaser = 40; }	// 最大40まで
+								rank = rank - 10;
+								if (rank < maxStartLaser){ rank = maxStartLaser; }	// 難易度調整
+								stage = stage + 1;	// ステージを1つ増やす
+								stageLabel.text = "STAGE "+stage;
+							}
+						}
+					}
+					scoreLabel.text = "SCORE : "+game.score;
+				}
+				// ------------ ■自機と敵のレーザービームの接触判定を行う -----------------
+				function hitCheckLaser(){
+					// 自機の判定用に仮のオブジェクトを作成しXY座標と幅を設定する
 
+					for(var i=0; i<maxLaser; i++){
+						if (enemyLaser[i].intersect(fighter)){	// 接触したらゲームオーバー
+							game.rootScene.backgroundColor = "red";	// ゲームの背景色を赤色に設定
+							game.stop();
+							alert("自機が破壊されました。もう駄目です。スコアは"+game.score+"点でした");
+							return;	// 以後の処理は行わないようにする
+						}
+					}
+				}
+
+				// ■爆発開始処理
+				function startBlast(sx, sy){
+					blast.flag = true;	// 爆風発生
+					blast.x = sx;
+					blast.y = sy;
+					blast.frame = 0;	// 爆風のアニメーションを最初の画像に
+				}
+				// ■爆発処理
+				function moveBlast(){
+					if (!blast.flag){ return; }	// 爆風がない場合は何もしない
+					blast.frame = blast.frame + 1;
+					if (blast.frame == 8){	// 爆風の枚数は8枚なので、それ以上の場合は爆風処理をしない
+						blast.flag = false;
+						blast.y = -999;
+					}
+				}
+			});
+		}
 	}
 	// ゲーム処理開始
 	game.start();
